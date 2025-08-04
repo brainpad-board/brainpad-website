da7516ceb16c7f749a354d4b7a039cf50bf41448
Gus Issa <gus.issa@ghielectronics.com>
2025-08-04 12:06:17 -0400

    change trailing slash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,6 +20,7 @@ const config = {
 
   url: `https://www.brainpad.com/`,
   baseUrl: `/`,
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
