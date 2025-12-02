9b5b8368cb7300dbb381aba77950131cff510ae1
Gus Issa <gusi@ghielectronics.com>
2025-12-02 13:55:52 +0400

    audit and fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,7 +28,7 @@ const config = {
   //projectName, 
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  //onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
